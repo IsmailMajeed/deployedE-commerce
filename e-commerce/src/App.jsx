27287da39abf836036b28c976310130be60808ce
Ismail <macbook@Ismails-MacBook-Pro.local>
2023-08-29 19:58:47 +0500

    make it better

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import AllProducts from "./Pages/AllProducts";
 import Features from "./Pages/Features";
 import Pricing from "./Pages/Pricing";
