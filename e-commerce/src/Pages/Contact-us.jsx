b6e96c80735f982b6ae7a5887977c0d5f0a9a231
Ismail <macbook@Ismails-MacBook-Pro.local>
2023-08-29 20:05:31 +0500

    make it better

--- e-commerce/src/Pages/Contact-us.jsx
+++ e-commerce/src/Pages/Contact-us.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function Contact_us() {
   return (
-    <div className='text-center'>
+    <div className='text-center flex flex-col gap-3'>
       <span>Name: M. Ismail</span>
       <span>Contact Number: <Link to="#">03111437753</Link></span>
     </div>
