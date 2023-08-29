430551c9db605e6d7b3135b68928b7106e088a9b
Ismail <macbook@Ismails-MacBook-Pro.local>
2023-08-29 23:41:16 +0500

    make it better

--- Server.js
+++ Server.js
@@ -62,7 +62,6 @@ function validateUser(req, res, next) {
 
 if (process.env.NODE_ENV == "production") {
   app.use(express.static("e-commerce/dist"));
-  app.use(express.static("Admin/dist"));
   // const path = require("path");
   // app.get("*", (req, res) => {
   //   res.sendFile(path.resolve(__dirname, 'e-commerce', 'dist', 'index.html'));
